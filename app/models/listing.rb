f2c06aa5b6392ff89df9bd538d0cf1520ecd7871
Thuong Ho <thuong.t.ho@gmail.com>
2014-06-16 01:11:20 -0700

    update listing model's validation

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,7 +4,8 @@ class Listing < ActiveRecord::Base
     has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" }, 
                     :default_url => "no_pic.jpg"
   else
-    has_attached_file :storage => :dropbox,
+    has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" },
+                      :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
                       :path => ":style/:id_:filename"
     validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/

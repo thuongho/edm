60b41a4b75902544d20218c18418de6b2955bbc5
Thuong Ho <thuong.t.ho@gmail.com>
2014-06-16 15:40:25 -0700

    associate listings with users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -7,11 +7,13 @@ class Listing < ActiveRecord::Base
     has_attached_file :image, :styles => { :medium => "200x>", :thumb => "100x100>" },
                       :storage => :dropbox,
                       :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
-                      :path => ":style/:id_:filename"
-    validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+                      :path => ":style/:id_:filename" 
   end
 
+  validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
   validates :name, :description, :price, presence: true
   validates :price, numericality: { greater_than: 0 }
-  validates_attachment_presence :image
+  validates :image, presence: true
+
+  belongs_to :user
 end
